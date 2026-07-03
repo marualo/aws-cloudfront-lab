27eecef0a005a1665f3ec0f120ba4fa13c0fcde8
marualo <marualonzo@hotmail.com>
2026-07-02 21:19:04 -0300

    Upload image to s3 with Terraform

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "bucket_name" {
-    description = "Name of the created bucket"
-    value       = aws_s3_bucket.images.bucket
+  description = "Name of the created bucket"
+  value       = aws_s3_bucket.images.bucket
 }
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -8,3 +8,10 @@ resource "aws_s3_bucket" "images" {
     ManagedBy   = "Terraform"
   }
 }
+
+resource "aws_s3_object" "image" {
+  bucket = aws_s3_bucket.images.bucket
+  key    = "images/cattos.jpg"
+  source = "../images/cattos.jpg"
+  etag   = filemd5("../images/cattos.jpg")
+}
